2fa652cd56146f22181d883f763a7c895634a3de
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-20 15:17:30 +0200

    print paragraph and length

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quis suscipit eveniet obcaecati tenetur, quisquam aliquam quia cum et adipisci commodi unde vero cumque nemo in, accusantium enim rem qui.';
+$length = strlen($text);
+?>
+
+<p><?php echo $text; ?></p>
+<p>Il testo contiene <?php echo $length; ?> caratteri.</p>
